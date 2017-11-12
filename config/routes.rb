604bc58bd4fd39be82c5a15a6601bb1551bca269
samahABf <samah.abudaia.1994@gmail.com>
2017-11-12 18:21:12 +0200

    3.8 add description of change

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  get 'simple_pages/about'
 
-  get 'simple_pages/contact'
+  get 'pages/about'
 
-  get 'simple_pages/index'
+  get 'pages/contact'
 
+  root 'pages#index'
   get 'pages/index'
-  root 'simple_pages#index'
+ 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
