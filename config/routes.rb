babc87780d6184d46452a255b33737cd2cd1caa1
samahABf <samah.abudaia.1994@gmail.com>
2017-11-18 22:55:17 +0200

    3.8 add description of change

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
-
 Rails.application.routes.draw do
-  resources :products
+  resources :products 
+
   get 'pages/about'
 
   get 'pages/contact'
 
- root 'products#index'
+  root 'pages#index'
   get 'pages/index'
  
 
