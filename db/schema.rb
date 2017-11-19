4d1ecb875e76fab6d7a7d6006494007f2f8190bd
samahABf <samah.abudaia.1994@gmail.com>
2017-11-19 14:01:51 +0200

    add color

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171119101946) do
+ActiveRecord::Schema.define(version: 20171119115620) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20171119101946) do
     t.datetime "updated_at", null: false
     t.string "colour"
     t.decimal "price"
+    t.string "color"
   end
 
 end
