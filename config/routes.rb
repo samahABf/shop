7ef448b27c7d0f6d8cf7348efca9dca22b250c8b
samahABf <samah.abudaia.1994@gmail.com>
2017-11-22 01:11:13 +0200

    added color field to products

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,6 @@ Rails.application.routes.draw do
   root 'pages#index'
   get 'pages/index'
  
-
+resources :orders, only: [:index, :show, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
