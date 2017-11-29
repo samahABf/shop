7d0d000752c57a06f2f5194f99960d61ca5081ee
samahABf <samah.abudaia.1994@gmail.com>
2017-11-29 13:20:23 +0200

    added search field to products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,7 @@
 class Product < ApplicationRecord
+	   def self.search(search_term)
+		like_string = Rails.env.production? ? "ilike" : "LIKE"
+		Product.where("name #{like_string} ?", "%#{search_term}%")
+    end
+
 end
