e5f985a25aac77e13ed4c4ac3cd9d73304af6457
samahABf <samah.abudaia.1994@gmail.com>
2017-12-02 11:07:53 +0200

    added color field to products

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "light-moon60@hotmail.com"
   layout 'mailer'
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,7 +5,7 @@ class UserMailer < ApplicationMailer
   def contact_form(email, name, message)
   @message = message
     mail(from: email,
-         to: 'your-email@example.com',
+         to: 'light-moon60@hotmail.com',
          subject: "A new contact form message from #{name}")
   end
 end
