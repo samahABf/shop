da02610ce6632061bd79ccdc2e076c4d83414679
samahABf <samah.abudaia.1994@gmail.com>
2017-12-03 11:37:57 +0200

    added color field to products

--- config/environment.rb
+++ config/environment.rb
@@ -6,11 +6,11 @@ require_relative 'application'
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
-  address: 'smtp.sendgrid.net',
+  address: 'smtp.gmail.com',
   port: '587',
   authentication: :plain,
-  user_name: ENV['SENDGRID_USERNAME'],
-  password: ENV['SENDGRID_PASSWORD'],
+  user_name: ENV['samah.aldafy.1994@gmail.com'],
+  password: ENV['0598290079'],
   domain: 'heroku.com',
   enable_starttls_auto: true
 }
