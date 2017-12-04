3d9182e14835aafccb74ecdd0f312981e7f800f5
samahABf <samah.abudaia.1994@gmail.com>
2017-12-04 06:40:29 +0200

    added color field to products

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,19 +1,20 @@
 class PagesController < ApplicationController
-  def index
-  end
-  def landing_page
-      @products = Product.limit(4)
-
-  end
-  UserMailer.contact_form(@email, @name, @message).deliver_now
-
-
+  
  def thank_you
  
   @name = params[:name]
   @email = params[:email]
   @message = params[:message]
   # the I18n.default_locale when a translation cannot be found).
+  UserMailer.contact_form(@email, @name, @message).deliver_now
 
  end
+  def index
+  end
+  def landing_page
+      @products = Product.limit(4)
+
+  end
+
+
 end
